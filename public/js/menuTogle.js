e77d0844a844e3372469270083ed3e878a9c9600
VadimDyliko <vadimdyliko@gmail.com>
2019-07-02 12:51:27 +0300

    Avatar image fix

--- public/js/menuTogle.js
+++ public/js/menuTogle.js
@@ -1,4 +1,4 @@
 function menuTogle() {
   let menuElement = document.querySelector('.left-side-bar');
-  menuElement.classList.toggle('left-side-bar--show')
+  menuElement.classList.toggle('left-side-bar--show');
 }
