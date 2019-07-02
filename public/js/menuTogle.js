305dc389afba0de42d4ff3ad9c765a29e58b47f5
VadimDyliko <vadimdyliko@gmail.com>
2019-07-02 19:09:28 +0300

    Fixed bugs

--- public/js/menuTogle.js
+++ public/js/menuTogle.js
@@ -1,4 +1,9 @@
 function menuTogle() {
   let menuElement = document.querySelector('.left-side-bar');
   menuElement.classList.toggle('left-side-bar--show');
+  if (menuElement.classList.contains('left-side-bar--show')){
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'scroll';
+  }
 }
