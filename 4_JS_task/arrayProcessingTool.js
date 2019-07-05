97e91696770db4f6275cd173e30c20d6590e5aa0
VadimDyliko <vadimdyliko@gmail.com>
2019-07-05 20:44:40 +0300

    Added arrayProcessionTool.js

--- 4_JS_task/arrayProcessingTool.js
+++ 4_JS_task/arrayProcessingTool.js
@@ -1,67 +1,79 @@
-var testArr = [1, 3, 7, 4, 6, 7, 8, 1, 2, 5, 7, 8, 90, 1]
+//var testArr = [1,3,7,4,6,7,8,1,2,5,7,8,90,1];
+//var testArr = [11, 9, 3, 2, 8]
 
-//getMaxSubSum([-1, 2, 3, -9]) = 5
-
-const arrayProcessingTool = {
-  subSum: function(arr){
-            var sum = 0;
-            var max = 0;
-            for(var i=0;i<arr.length;i++){
-                for(var p=i;p<arr.length;p++){
-                    sum += arr[p];
-                    if(sum < 0){
-                        continue;
-                    }else if(sum > max){
-                        max = sum;
-                    }
-                }
-                sum = 0;
-            }
-            return max
-        },
-    search: function(arr) {
-      //sortFromMaxToMin
-      var arrMaxMin = arr.concat()
-      arrMaxMin.sort((a,b)=>{
-        if(a<b) return 1;
-        if(a>b) return -1;
-      })
-      //sortFromMinToMax
-      var arrMinMax = arr.concat()
-      arrMinMax.sort((a,b)=>{
-        if(a<b) return -1;
-        if(a>b) return 1;
-      })
-      function searchMediane(arr) {
-        console.log(arr.length%2);
-        if (!arr.length%2){
-          let index = (arr.length/2)+1
-          console.log(index);
-          return index
-        } else {
-
+var arrayProcessingTool = {
+  subSumOn2: function(arr){
+    d
+    var sum = 0, max = 0
+    for(var i=0;i<arr.length;i++){
+      for(var j=i;j<arr.length;j++){
+        sum += arr[j];
+        if(sum < 0){
+          continue;
+        }else if(sum > max){
+          max = sum;
         }
       }
-      searchMediane(arrMaxMin)
-      return (`${arrMaxMin} ${arrMinMax}`)
-    },
-
-    selectionTask: function(arr) {
-      var startIndex, endIndex
-      for (var i=0; i<arr.length; i++){
-        if (arr[i]>arr[i+1]){
-          endIndex = i
-        } else {
-          startIndex = i
-        }
-      }
-      console.log(startIndex, endIndex);
+      sum = 0;
     }
-}
+    return max
+  },
 
-console.log(arrayProcessingTool.selectionTask(testArr));
-// console.log(arrayProcessingTool.subSum(testArr));
-// console.log(arrayProcessingTool.subSum([-1, 2, 3, -9]));
-// console.log(arrayProcessingTool.subSum([2, -1, 2, 3, -9]));
-// console.log(arrayProcessingTool.subSum([-1, 2, 3, -9, 11]));
-// console.log(arrayProcessingTool.subSum([-100, -9, -2, -3, -5]));
+
+  subSumOn: function(arr) {
+    var max=0, sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+      max = Math.max(max, sum);
+      if (sum < 0) sum = 0;
+    }
+    return max;
+  },
+
+
+  searchMediane: function(arr) {
+    var minMediane, maxMediane;
+    var maxMinArr = arr.concat().sort((a,b)=>{
+      if(a<b) return 1;
+      if(a>b) return -1;
+    })
+    var minMaxArr = arr.concat().sort((a,b)=>{
+      if(a<b) return -1;
+      if(a>b) return 1;
+    })
+    console.log(minMaxArr);
+    console.log(maxMinArr);
+    if (arr.length%2===0) {
+      maxMediane = (minMaxArr[Math.floor(arr.length/2)]+minMaxArr[Math.floor(arr.length/2)-1])/2
+      minMediane = (maxMinArr[Math.floor(arr.length/2)]+maxMinArr[Math.floor(arr.length/2)-1])/2
+      return (
+        'max mediane:'+maxMediane+'\n min mediane:'+minMediane
+      )
+    } else {
+      maxMediane = minMaxArr[Math.floor(arr.length/2)]
+      minMediane = maxMinArr[Math.floor(arr.length/2)]
+      return (
+        'max mediane:'+maxMediane+'\nmin mediane:'+minMediane
+      )
+    }
+  },
+
+
+  selectionTask: function(arr) {
+    var sequenceArr = [arr[0]], temporaryArr = [arr[0]];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] >= arr[i-1]) {
+            temporaryArr.push(arr[i]);
+        } else {
+            if (temporaryArr.length > sequenceArr.length) {
+                sequenceArr = temporaryArr;
+            }
+            temporaryArr = [arr[i]];
+        }
+    }
+    if (temporaryArr.length > sequenceArr.length) {
+        sequenceArr = temporaryArr;
+    }
+    return sequenceArr;
+  }
+}
